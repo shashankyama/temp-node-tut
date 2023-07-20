b9887a40e170d71d87be0c6958822a87f89ca2bb
bunny yama <yamashashank@gmail.com>
2023-07-20 16:03:31 -0500

    readstream,

--- 17-http-stream.js
+++ 17-http-stream.js
@@ -14,13 +14,3 @@ http
     });
   })
   .listen(5000);
-
-// var http = require("http");
-// var fs = require("fs");
-
-// http
-//   .createServer(function (req, res) {
-//     const text = fs.readFileSync("./content/big.txt", "utf8");
-//     res.end(text);
-//   })
-//   .listen(4000);
